7a5c777c2bbfc3981e7cc4000e24b570741c9caf
lboltz23 <lacey.boltz@gmail.com>
2024-04-11 16:01:29 -0500

    indentation

--- run.py
+++ run.py
@@ -26,12 +26,7 @@ def main():
                 print("no url", err)
             else:
                 print("invalid url", err)
-        else:
-            if data['body'] == "":
-                print("no body information found")
-            elif data['title'] == "":
-                print("no title found in article")
-            
+                
             file_scrape.write_rawfile(index, data)  #writing all the data besides the body into the raw file using the Write_File() instance
             file_scrape.write_processedfile(index, data['body']) #writing the body content into the processed file using the Write_File() instance
             Summary = summary.get_summary(data['body'])
